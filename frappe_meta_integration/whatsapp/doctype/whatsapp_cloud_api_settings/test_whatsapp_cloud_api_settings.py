17265126babbaed46dd63212c461ec1126baa116
sherinkr <sherinkrply@gmail.com>
2022-06-17 15:12:50 +0530

    feat: WhatsApp Cloud API Integration

---
+++ frappe_meta_integration/whatsapp/doctype/whatsapp_cloud_api_settings/test_whatsapp_cloud_api_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, efeone Pvt. Ltd. and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestWhatsAppCloudAPISettings(unittest.TestCase):
+	pass
---
+++ frappe_meta_integration/whatsapp/doctype/whatsapp_communication/test_whatsapp_communication.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, efeone Pvt. Ltd. and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestWhatsAppCommunication(unittest.TestCase):
+	pass
