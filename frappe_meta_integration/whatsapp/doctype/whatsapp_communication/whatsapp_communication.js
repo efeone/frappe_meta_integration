00f26ae02a6b1f5c89a3f0a107b0f79fc0aa3b32
sherinkr <sherinkrply@gmail.com>
2022-06-30 15:35:40 +0530

    feat: WhatsApp Message Template Configuration and Messaging

--- frappe_meta_integration/whatsapp/doctype/whatsapp_communication/whatsapp_communication.js
+++ frappe_meta_integration/whatsapp/doctype/whatsapp_communication/whatsapp_communication.js
@@ -2,6 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('WhatsApp Communication', {
+  setup: function(frm){
+    frm.get_field("parameters").grid.cannot_add_rows = true;
+    frm.refresh_field("parameters");
+    frm.set_query('whatsapp_message_template', () => {
+        return {
+            filters: {
+                is_existing_template: 1
+            }
+        }
+    })
+  },
   refresh: function(frm) {
     if(!frm.is_new()){
       make_custom_buttons(frm);
@@ -10,7 +21,27 @@ frappe.ui.form.on('WhatsApp Communication', {
       let wrapper = frm.get_field("preview_html_rendered").$wrapper;
       wrapper.html(frm.doc.preview_html);
     }
-	}
+	},
+  whatsapp_message_template: function(frm){
+    set_template_parameters(frm);
+  }
+});
+
+frappe.ui.form.on('WhatsApp Message Template Item', {
+  parameters_remove: function(frm, cdt, cdn){
+    frappe.show_alert({
+      message:__('You are not allowed to add/remove parameter Manually!. Changes will be reverted.'),
+      indicator:'red'
+    }, 5);
+    set_template_parameters(frm);
+  },
+  parameters_add: function(frm, cdt, cdn){
+    frappe.show_alert({
+      message:__('You are not allowed to add/remove parameter Manually!. Changes will be reverted.'),
+      indicator:'red'
+    }, 5);
+    set_template_parameters(frm);
+  }
 });
 
 function make_custom_buttons(frm){
@@ -47,6 +78,26 @@ function create_upload_media_button(frm){
           indicator: "green",
         });
       });
+    });
+  }
+}
+
+function set_template_parameters(frm){
+  if(frm.doc.whatsapp_message_template){
+    frappe.call({
+      method: 'frappe_meta_integration.whatsapp.doctype.whatsapp_message_template.whatsapp_message_template.set_template_parameters',
+      args: {
+        "whatsapp_message_template": frm.doc.whatsapp_message_template
+      },
+      callback: function(r) {
+        frm.clear_table('parameters');
+        for(var i=0; i< r.message.length; i++){
+          let row =frm.add_child('parameters',{
+            parameter: r.message[i].parameter
+          });
+        }
+        frm.save()
+      }
     });
   }
 }
