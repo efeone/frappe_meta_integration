d1ea73cd6d5bd9fe039b7552a78d5d5a9e23ade7
sherinkr <sherinkrply@gmail.com>
2022-07-01 12:34:59 +0530

    feat: Added button to verify token in Cloud API Settings

--- frappe_meta_integration/whatsapp/doctype/whatsapp_cloud_api_settings/whatsapp_cloud_api_settings.js
+++ frappe_meta_integration/whatsapp/doctype/whatsapp_cloud_api_settings/whatsapp_cloud_api_settings.js
@@ -2,7 +2,42 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('WhatsApp Cloud API Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+    if(frm.doc.access_token && frm.doc.phone_number_id){
+			frm.add_custom_button('Verify Token', () => {
+		    set_primary_action(frm);
+		  }).addClass("btn-primary");
+    }
+	}
 });
+
+function set_primary_action(frm){
+  frappe.prompt({
+      label: 'WhatsApp Number',
+      fieldname: 'phone_number',
+      fieldtype: 'Data',
+      description: 'WhatsApp number with country code(eg: 91)',
+      reqd: 1
+  }, (values) => {
+      send_verify_message(frm, values.phone_number)
+  })
+}
+
+function send_verify_message(frm, phone_number){
+  frappe.call({
+    method: 'frappe_meta_integration.whatsapp.doctype.whatsapp_cloud_api_settings.whatsapp_cloud_api_settings.send_test_message',
+    args: {
+      "phone_number": phone_number
+    },
+    callback: function(r) {
+      if(r && r.message){
+        frappe.msgprint({
+            title: __('Success'),
+            message: __("WhatsApp Account successfully configured. <br> Message ID : {0}",[r.message])
+        });
+      }
+    },
+    freeze: true,
+    freeze_message: ('Sending WhatsApp test Message.!!')
+  });
+}
